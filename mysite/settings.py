d2ecf18a46e71eeb058df6cdbeb099a659fe474d
ejnarvala <ejnar123@gmail.com>
2017-06-27 21:56:52 -0400

    again

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,8 +131,13 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
