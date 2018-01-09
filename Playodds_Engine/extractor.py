53fec7ab7b065aec0bf6c01b60e7d452935124e6
ejnarvala <ejnar123@gmail.com>
2018-01-09 18:11:04 -0500

    merging

--- Playodds_Engine/extractor.py
+++
@@ -1,9 +0,0 @@
-import urllib.parse as urlparse
-import re
-
-
-def extract_leagueId(url):
-	parsed = urlparse.urlparse(url)
-	leagueId_str = (urlparse.parse_qs(parsed.query)['leagueId'])[0] #throws KeyError
-	leagueId = int(re.sub("[^0-9]", "", leagueId_str)); #throws ValueError
-	return leagueId
