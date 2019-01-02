8e0a76d920f373bd2fada9fbf9b1256d6c34ffbb
ejnarvala <ejnar123@gmail.com>
2019-01-02 14:49:52 -0500

    new method of backend/frontend interaction

--- playodds.py
+++ playodds.py
@@ -1,39 +1,66 @@
 from flask import Flask, request, render_template, abort, jsonify, make_response
 from PlayOddsEngine import simulator, extractor
 from PlayOddsEngine.extractor import *
+from rq import Queue
+from rq.job import Job
+from worker import conn
 
 app = Flask(__name__)
+q = Queue(connection=conn)
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def index():
-	error = None
 	if(request.method == 'POST'):
-		url = str(request.form['espn-url'])
-		try:
-			league_id = extractor.extract_leagueId(url)
-		except Exception as e:
-			return render_template('index.html', error=str(e))
-		try:
-			week = str(request.form['inputWeek'])
-			year = str(request.form['inputYear'])
-			week = int(week) if (not week == 'Week') else None
-			year = int(year) if (not year == 'Year') else simulator.CURRENT_SEASON
-			results = simulator.simulate(league_id, year, week)
-		except SimulatorError as e:
-			return render_template('index', error=str(e))
-		except Exception as e:
-			print(str(e))
-			error = 'Something went wrong!'
-			return render_template('index.html', error=error)
-		
 		return render_template('results.html', results=results)
+	return render_template('index.html')
 
-	return render_template('index.html', error=error)
+@app.route("/simulate", methods=['POST'])
+def simulate():
+	print('got something')
+	output = {
+		'job_id': None,
+		'error': None
+	}
+	try:
+		payload = request.get_json()
+		print('payload:',payload)
+		league_id = extractor.extract_leagueId(payload['espnURL'])
+		week = str(payload['week'])
+		year = str(payload['year'])
+		week = int(week) if (not week == 'Week') else None
+		year = int(year) if (not year == 'Year') else simulator.CURRENT_SEASON
+		print('sim running...')
+		results = simulator.simulate(league_id, year, week)
+		job = q.enqueue_call(func=simulator.simulate, args=(league_id, year, week), timeout=1000)
+		output['job_id'] = job.get_id()
+	except Exception as e:
+		print('Error:',str(e))
+		error = str(e)
+		output['error'] = error
+	
+	print(output)
+	return jsonify(output)
+
+
+@app.route("/job_status/<int:job_id>")
+def job_status(job_id):
+    q = Queue()
+    job = q.fetch_job(job_id)
+    if job is None:
+        response = {'status': 'unknown'}
+    else:
+        response = {
+            'status': job.get_status(),
+            'result': job.result
+        }
+    return jsonify(response)
+
 
 @app.route("/info")
 def info():
 	return render_template('info.html')
+
 
 
 @app.route("/api/v1.0/leagueid/<int:api_league_id>", methods=['GET'])
@@ -46,7 +73,6 @@ def get_results_leagueid(api_league_id):
 		return make_response(jsonify({'error':error}), 500)
 
 	return jsonify(results)
-
 
 
 @app.route("/api/v1.0/url/<api_url>", methods=['GET'])
