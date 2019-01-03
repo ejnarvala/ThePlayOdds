ef6d85743450aa5f26bb02aeb75aca9dc6b507f3
ejnarvala <ejnar123@gmail.com>
2019-01-03 01:47:42 -0500

    updated job status logic for setting payload, deactivated api for now

--- playodds.py
+++ playodds.py
@@ -4,6 +4,7 @@ from PlayOddsEngine.extractor import *
 from rq import Queue
 from rq.job import Job
 from worker import conn
+import time
 
 app = Flask(__name__)
 q = Queue(connection=conn)
@@ -12,7 +13,6 @@ q = Queue(connection=conn)
 @app.route("/")
 def index():
 	return render_template('index.html')
-
 
 
 @app.route("/simulate", methods=['POST'])
@@ -32,43 +32,40 @@ def simulate():
 		year = int(year) if (not year == 'Year') else simulator.CURRENT_SEASON
 		print('sim running...')
 		# results = simulator.simulate(league_id, year, week)
-		job = q.enqueue_call(func=simulator.simulate, args=(league_id, year, week), timeout=1000)
+		job = q.enqueue_call(func=simulator.simulate, args=(league_id, year, week), timeout='5m', result_ttl='24h')
 		output['job_id'] = job.get_id()
 	except Exception as e:
 		print('Error:',str(e))
 		error = str(e)
 		output['error'] = error
 	
-	print(output)
 	return jsonify(output)
 
 @app.route("/results/<job_id>")
 def results(job_id):
-    job = q.fetch_job(job_id)
-    if job is None:
-        response = {
-        	'status': 'unknown',
-        	}
-    else:
-        response = {
-        	'job_id': job_id,
-            'status': job.get_status(),
-            'result': job.result
-        }
-    return render_template('results.html', payload=response)
+	return render_template('results.html', payload={'job_id': job_id})
 
 
 @app.route("/job_status/<job_id>")
 def job_status(job_id):
-    job = q.fetch_job(job_id)
-    if job is None:
-        response = {'status': 'unknown'}
-    else:
-        response = {
-            'status': job.get_status(),
-            'result': job.result
-        }
-    return jsonify(response)
+
+	response = {
+		'valid_job': False,
+		'job_id': job_id,
+		'status': "",
+		'result': None
+	}
+	job = q.fetch_job(job_id)
+	
+	if job is None:
+		response['status'] = 'job not found!'
+	else:
+		if not job.is_failed:
+			response['valid_job'] = True
+		response['status'] = job.get_status()
+		response['result'] = job.result
+
+	return jsonify(response)
 
 
 @app.route("/info")
@@ -76,31 +73,30 @@ def info():
 	return render_template('info.html')
 
 
+# @app.route("/api/v1.0/leagueid/<int:api_league_id>", methods=['GET'])
+# def get_results_leagueid(api_league_id):
+# 	try:
+# 		results = simulator.simulate(api_league_id)
+# 	except Exception as e:
+# 		# print(str(e))
+# 		error = 'Simulator Error has Occurred! :O'
+# 		return make_response(jsonify({'error':error}), 500)
 
-@app.route("/api/v1.0/leagueid/<int:api_league_id>", methods=['GET'])
-def get_results_leagueid(api_league_id):
-	try:
-		results = simulator.simulate(api_league_id)
-	except Exception as e:
-		# print(str(e))
-		error = 'Simulator Error has Occurred! :O'
-		return make_response(jsonify({'error':error}), 500)
-
-	return jsonify(results)
+# 	return jsonify(results)
 
 
-@app.route("/api/v1.0/url/<api_url>", methods=['GET'])
-def get_results_url(api_url):
-	url_extracted, extracted_data = extractor.extract_leagueId(api_url) # returns true/fase, leagueId/error
-	if(not url_extracted):
-		return make_response(jsonify({'error': extracted_data}), 400)
-	try:
-		results = simulator.simulate(leagueId)
-	except Exception as e:
-		# print(str(e))
-		error = 'Simulator Error has Occurred!'
-		return make_response(jsonify({'error':error}), 500)
-	return jsonify(results)
+# @app.route("/api/v1.0/url/<api_url>", methods=['GET'])
+# def get_results_url(api_url):
+# 	url_extracted, extracted_data = extractor.extract_leagueId(api_url) # returns true/fase, leagueId/error
+# 	if(not url_extracted):
+# 		return make_response(jsonify({'error': extracted_data}), 400)
+# 	try:
+# 		results = simulator.simulate(leagueId)
+# 	except Exception as e:
+# 		# print(str(e))
+# 		error = 'Simulator Error has Occurred!'
+# 		return make_response(jsonify({'error':error}), 500)
+# 	return jsonify(results)
 
 
 
